2cd10ec302c700da9fe84182481c1b6566a1f52c
codewheezy <uchennaibe95@gmail.com>
2021-11-19 02:26:44 -0800

    added build file to github

---
+++ build/precache-manifest.7ed12bf713a4f0f0a194c6e5eb6e874f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30b2740420320144fb1ccbd60f38d943",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d68ccbbae48297834343",
+    "url": "/static/css/2.1781c263.chunk.css"
+  },
+  {
+    "revision": "34a656a69564124b14b3",
+    "url": "/static/css/main.7a3b3ecb.chunk.css"
+  },
+  {
+    "revision": "d68ccbbae48297834343",
+    "url": "/static/js/2.3e741fc7.chunk.js"
+  },
+  {
+    "revision": "a2334ff9bc68c321bce67af5ebb4ec33",
+    "url": "/static/js/2.3e741fc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34a656a69564124b14b3",
+    "url": "/static/js/main.13c5dc9a.chunk.js"
+  },
+  {
+    "revision": "9a5913a1e8424b1c1c6ab8de937079b1",
+    "url": "/static/js/main.13c5dc9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80c87aabfa44130f16c2",
+    "url": "/static/js/runtime-main.b3b5c711.js"
+  },
+  {
+    "revision": "a127e2c329dffa0fc821a733b844c554",
+    "url": "/static/media/15.a127e2c3.png"
+  },
+  {
+    "revision": "de7140440627e9d0b1b842b582cc00ca",
+    "url": "/static/media/blogging 1.de714044.png"
+  }
+]);
